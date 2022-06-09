049b2b59f5d85df38f461502d19615481f32f35c
mnbst <47024101+mnbst@users.noreply.github.com>
2022-06-09 09:29:42 +0900

    health check削除

--- backend/src/app/settings.py
+++ backend/src/app/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "src.app.middleware.HealthCheckMiddleware",
 ]
 
 ROOT_URLCONF = "app.urls"
